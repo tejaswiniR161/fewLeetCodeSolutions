6b95d5eade6a1fce48fc3e35429c62cc9042ab96
tejaswiniR161 <tejaswinigowda1996@gmail.com>
2020-09-01 13:15:56 -0400

    some mistake in spiral

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -10,8 +10,6 @@ class Solution:
 
             for j in range(k,c-k):
                 print("k and j ",k, j)
-                s=k
-                t=j
                 ans.append(matrix[k][j])
                 print("1st ",matrix[k][j])
             for j in range(k+1,r-k-1):
@@ -19,17 +17,16 @@ class Solution:
                 print("2nd ",matrix[j][c-k-1])
             for j in range(c-k-1,k-1,-1):
                 print("r-1-k = ",r-1-k," j = ",j)
-                if s!=r-1-k and t!=j:
-                    ans.append(matrix[r-1-k][j])
-                    print("3rd ",matrix[r-1-k][j])
+                ans.append(matrix[r-1-k][j])
+                print("3rd ",matrix[r-1-k][j])
             for j in range(r-k-2,k,-1):
                 ans.append(matrix[j][k])
                 print("4th ",matrix[j][k])
 
         return ans
         
-matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]]
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
+#matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]]
+matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 s=Solution()
 print(s.spiralOrder(matrix))
 
