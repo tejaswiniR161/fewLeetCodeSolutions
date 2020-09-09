17fbfe43cc838310b16266cb7c1d4cb6d73351e9
tejaswiniR161 <tejaswinigowda1996@gmail.com>
2020-09-09 12:07:23 -0400

    whatever

--- matrix/rotate_90.py
+++ matrix/rotate_90.py
@@ -15,7 +15,7 @@ for i in range(0,n):
         #print("i,j ",i,j)
         print(matrix[i][j]," ",end="")
     print() """
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
+""" matrix=[[1,2,3],[4,5,6],[7,8,9]]
 n=len(matrix)
 tn=n
 for i in range(0,int(n/2)):
@@ -32,4 +32,33 @@ for i in range(0,int(n/2)):
     left=[j[i] for j in matrix]
     print(left)
 
+    """ 
+
+matrix=[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+def display(matrix123):
+    for i in range(len(matrix123)):
+        for j in range(len(matrix123)):
+            print(matrix123[i][j],end=" ")
+        print()
+
+def rotate_90(matrix):
+    n=len(matrix)
+    for i in range(n):
+        for j in range(n):
+            print("i, j",i,j)
+            print(matrix[i][j])
+            print(matrix[j][i])
+            t=matrix[i][j]
+            matrix[i][j]=matrix[j][i]
+            matrix[j][i]=t
+            print(" after changing i, j",i,j)
+            print(matrix[i][j])
+            print(matrix[j][i])
     
+    for i in range(n):
+        for j in range(n):
+            print(matrix[i][j],end=" ")
+        print()
+
+rotate_90(matrix)
